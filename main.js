fa383a6cc629ce38ea33489787116bd474a9d55b
Flaviabel <flaviamanfredi521@gmail.com>
2022-10-20 10:49:02 +0200

    ejercicio 1-4

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+1.1
+
+let array = ["January", "February", "Marj", "April", "Mei"]
+array[2] = "March";
+array[4] = "May";
+console.log(array)
+
+1.2
+let newMonths = ["June", "July", "August", "September"]
+let totalMonths = array.concat(newMonths);
+console.log(totalMonths)
+
+1.3
+totalMonths.push("October", "November", "December")
+console.log(totalMonths)
+
+4
+
+function  changeArray(num1,num2,num3){
+        let totalMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            if (num1>num2){
+               totalMonths.pop()
+                console.log(totalMonths)
+            }
+             else if(num2>num3 && num2<10){
+             totalMonths.shift()
+                    console.log(totalMonths)
+            }
+             else if (num2>num3 || num2>num1){ 
+             totalMonths.splice(12,0,"October")
+             console.log(totalMonths)
+                }
+                    
+            else {
+                console.log("Bad luck!")
+        }
+        }
+        console.log(changeArray(9,7,5))
 
 5.1
 function second(w){
